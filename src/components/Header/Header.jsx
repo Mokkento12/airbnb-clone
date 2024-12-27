593426408e57d3d4d7c8eb7b8f5d64e81cabcb61
roman alekseev <roma.alekseev.90@bk.ru>
2024-12-27 22:26:42 +0300

    Добавлено лого и стили для Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
+import Logo from "../Logo/Logo";
 import "./style.scss";
 
 function Header() {
-	return <header className="header">Header</header>;
+	return (
+		<header className="header">
+			<div className="container">
+				<div className="header__row">
+					<div className="header__logo">
+						<Logo />
+					</div>
+					<div className="header__search">SEARCH</div>
+					<div className="header__logo">USER MENU</div>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
