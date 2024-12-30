ba629646cd5e0a22392f52f0bdce11692c0ab593
roman alekseev <roma.alekseev.90@bk.ru>
2024-12-30 14:58:38 +0300

    Добавлена разметка для UserMenu

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+const UserMenu = () => {
+	return (
+		<div className="user-menu">
+			<div className="user-menu__home">Airbnb your home</div>
+			<div className="user-menu__local">Глобус</div>
+			<div className="user-menu__user">USER</div>
+		</div>
+	);
+};
+
+export default UserMenu;
